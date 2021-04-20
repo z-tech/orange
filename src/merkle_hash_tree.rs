551f85c8e0ac369ce2a425a2d107476696683812
Andrew Zitek <zitek936@gmail.com>
2021-04-19 20:20:36 -0400

    hash_at and is frozen stubs

--- src/merkle_hash_tree.rs
+++ src/merkle_hash_tree.rs
@@ -8,18 +8,21 @@ pub mod test_data;
 const MHT_LEAF_PREFIX: u8 = 0;
 const MHT_NODE_PREFIX: u8 = 1;
 
+fn min_num_bits(x: isize) -> isize {
+    /*
+        there's an isize::BITS function that does this, but it is only a "nightly" build,
+        so just working around that for now with count_ones() and count_zeros()
+    */
+    let total_bits: u32 = x.count_ones() + x.count_zeros();
+    return (total_bits - x.leading_zeros()) as isize;
+}
+
 fn depth(store: &impl Storer) -> isize {
     let mut width: isize = store.width();
     if width == 0 {
         return -1;
     }
-    width -= 1;
-    /*
-        there's an isize::BITS function that does this, but it is only a "nightly" build,
-        so just working around that for now with count_ones() and count_zeros()
-    */
-    let total_bits: u32 = width.count_ones() + width.count_zeros();
-    return (total_bits - width.leading_zeros()) as isize;
+    return min_num_bits(width - 1);
 }
 
 fn root(store: &impl Storer) -> Vec<u8> {
@@ -69,6 +72,31 @@ fn append_hash(store: &mut impl Storer, leaf_hash: Vec<u8>) {
     }
 }
 
+fn is_frozen(layer: isize, index: isize, at: isize) -> bool {
+    let a: isize = 1 << layer;
+    return at >= index*a+a-1;
+}
+
+fn hash_at(store: &mut impl Storer, l: isize, r: isize, at: isize) -> Vec<u8> {
+    if r == l {
+        return store.get(0, r).unwrap();
+    }
+
+    let layer: isize = min_num_bits(r-l); // height of subtree
+    let a: isize = 1 << layer; // width of subtree
+
+    if at >= l+a-1 || at == store.width()-1 {
+        return store.get(layer, l/a).unwrap();
+    }
+
+    let k: isize = a / 2;
+    let mut c: Vec<u8> = Vec::new();
+    c.push(MHT_NODE_PREFIX);
+    c.extend(hash_at(store, l, l+k-1, at).iter().cloned());
+    c.extend(hash_at(store, l+k, r, at).iter().cloned());
+    return digest(Algorithm::SHA256, &c);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -97,5 +125,11 @@ mod tests {
         let value: Vec<u8> = "my value".as_bytes().to_vec();
         append(&mut ms, value.to_vec());
         assert_eq!(hash_leaf(value), root(&ms));
+    }
+    #[test]
+    fn test_is_frozen() {
+        assert_eq!(is_frozen(0, 0, 0), true);
+        assert_eq!(is_frozen(6, 0, 7), false);
+        // TODO(z-tech) assert_eq!(is_frozen(7, 0, 7), true);
     }
 }
